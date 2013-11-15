ce80ccee8a7a19a173e01ce633abcf78fe4f2162
Austin J. Alexander <austinjalexander@gmail.com>
2013-11-15 17:14:45 -0500

    bootstrap and google and links

---
+++ ecma.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+
+	// function to repeat
+	function emptyAndAppend(data, rand_num) {
+	 		$("#place").empty();
+  		$("#place").append(data[rand_num].place);
+  		$("#caption").empty();
+  		$("#caption").append('"' + data[rand_num].caption + '"');
+  		$("#google_it").attr("href", "https://www.google.com/search?q=" + data[rand_num].name + "+Boston")
+	}
+
+	// on load	
+	$.getJSON("places.json", function(data) {
+		var rand_num = Math.floor(Math.random()*data.length);
+		emptyAndAppend(data, rand_num);
+	 	
+	 	// on click
+	 	$("#nah").click(function(){
+	 		rand_num = Math.floor(Math.random()*data.length);
+	 		emptyAndAppend(data, rand_num);
+  	});	
+	});
+
+});
